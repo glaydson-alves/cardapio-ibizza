e5dfba88f883adb27c60fd6b130619bb9dc99488
glaydson-alves <glaydson.alves36@gmail.com>
2024-06-18 23:01:41 -0300

    feat: add firenase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
@@ -7,7 +6,7 @@ const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DATABASE_URL,
-  projectId: PROJECT_ID,
+  projectId: "cardapio-online-4c9f0",
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID
